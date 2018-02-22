36887bd10b296ff8bb7f968afb0c68d52c816071
Tadas Šukys <tadas.sukys@visma.com>
2018-02-22 18:39:40 +0200

    Added Copy-Photos.ps1 and Get-Remote-File-Content.ps1

---
+++ Copy-Photos.ps1
@@ -0,0 +1,36 @@
+﻿#
+# The script scans a source directory of .jpg files, gets DateTaken date value, makes a new file name in format yyyyMMdd-HHmmss.jpg
+# and copies the file to destination directory.
+#
+
+param (
+    [string]$SourceDir = "",
+    [string]$DestinationDir = ""
+)
+
+[reflection.assembly]::LoadFile("C:\Windows\Microsoft.NET\Framework\v4.0.30319\System.Drawing.dll") 
+
+$photos = Get-ChildItem -Path "$SourceDir\*.jpg" | Select-Object -Property FullName, Name, BaseName, Extension
+
+Write-Host $photos.Count " photos found on " $SourceDir
+
+foreach ($p in $photos) {
+    $img = New-Object System.Drawing.Bitmap($p.FullName)
+    
+    Write-Host ($photos.IndexOf($p) + 1) "/" ($photos.Count) ": " $p.FullName
+    try
+    {
+        $prop = $img.GetPropertyItem(36867).Value
+        [string]$dateString = [System.Text.Encoding]::ASCII.GetString($prop)
+        [string]$dateTaken = [datetime]::ParseExact($dateString,"yyyy:MM:dd HH:mm:ss`0",$Null).ToString('yyyyMMdd-HHmmss')
+        $newFileName = $dateTaken + ".jpg"
+        
+        Copy-Item -Path $p.FullName -Destination ($DestinationDir + $newFileName)
+    }
+    catch
+    {
+        Write-Host -ForegroundColor White -BackgroundColor Red "Unable to get property of the file $p"
+    }
+
+    $img.Dispose()
+}
---
+++ Get-Remote-File-Content.ps1
@@ -0,0 +1,27 @@
+﻿#
+# The script reads remote file (via http[s] or ftp[s]) and sends the content to "output stream"
+# Usage example: 
+# .\Get-Remote-File-Content.ps1 -RemoteFileUrl ftp://ftp.gnu.org/pub/gnu/emacs/windows/README
+# .\Get-Remote-File-Content.ps1 -RemoteFileUrl https://www.gnu.org/licenses/gpl-3.0.txt | more
+#
+
+param (
+    [string]$RemoteFileUrl = "",
+    [string]$UserName = "",
+    [string]$Password = ""
+)
+
+if (!$Password -and $UserName -and $UserName.ToUpperInvariant() -ne "ANONYMOUS") {
+    $response = Read-host "Enter the password:" -AsSecureString 
+    $Password = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($response))
+}
+
+$tempFilePath = $env:TEMP + "\" +[guid]::NewGuid()
+
+$webClient = New-Object System.Net.WebClient
+$webClient.Credentials = New-Object System.Net.NetworkCredential($UserName,$Password)
+$webClient.DownloadFile($RemoteFileUrl, $tempFilePath)
+
+Get-Content $tempFilePath | Write-Output
+
+Remove-Item $tempFilePath
